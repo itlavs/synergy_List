9fb71e308f523d4a64deda1bd6b81c6486a0d203
lavsexpert <lavrov-sergey@yandex.ru>
2020-10-09 09:48:38 +0300

    init project + layout

---
+++ app/src/main/java/club/plus1/list/MainActivity.java
@@ -0,0 +1,20 @@
+package club.plus1.list;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // ImagePhoto - паскалевская нотация
+        // imagePhoto - верблюжья нотация
+        // image_photo - змеиная нотация
+        // image-photo - кебаб нотация
+    }
+}
